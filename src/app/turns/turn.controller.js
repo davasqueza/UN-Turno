2bc11792b24e48f7c35313571bf120cb7c3d8f6c
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-11-06 17:05:13 -0500

    Flujo de agregar/ver turnos

--- src/app/turns/turn.controller.js
+++ src/app/turns/turn.controller.js
@@ -54,5 +54,6 @@
 		}
 
 		loadTurns();
+    loadFilters();
 	}
 })();
--- src/app/turns/turn.service.js
+++ src/app/turns/turn.service.js
@@ -1,54 +1,54 @@
-(function() {
-	'use strict';
+(function () {
+  'use strict';
 
-	angular
-		.module('unTurno')
-		.service('TurnsService', TurnsService);
+  angular
+    .module('unTurno')
+    .service('TurnsService', TurnsService);
 
-	/** @ngInject */
-	function TurnsService($resource,backend_url, $firebaseArray) {
-		var service = {
-			getAllTurns: getAllTurns,
-			getTurnsByID: getTurnsByID,
-			createTurn: createTurn,
-			updateTurn: updateTurn,
-			deleteTurn: deleteTurn
-		};
+  /** @ngInject */
+  function TurnsService($resource, backend_url, $firebaseArray) {
+    var service = {
+      getAllTurns: getAllTurns,
+      getTurnsByID: getTurnsByID,
+      createTurn: createTurn,
+      updateTurn: updateTurn,
+      deleteTurn: deleteTurn
+    };
 
-		var TurnResource =  $resource(backend_url+'/turns/:id', {}, {
-			query: { method: 'GET', isArray: true },
-			create: { method: 'POST' },
-			show: { method: 'GET' },
-			update: { method: 'PUT', params: {id: '@id'} },
-			delete: { method: 'DELETE', params: {id: '@id'} }
-		});
+    var TurnResource = $resource(backend_url + '/turns/:id', {}, {
+      query: {method: 'GET', isArray: true},
+      create: {method: 'POST'},
+      show: {method: 'GET'},
+      update: {method: 'PUT', params: {id: '@id'}},
+      delete: {method: 'DELETE', params: {id: '@id'}}
+    });
 
     var database = firebase.database().ref().child("turns");
 
-		return service;
+    return service;
 
-		function getAllTurns(){
-		  return $firebaseArray(database);
-		}
+    function getAllTurns() {
+      return $firebaseArray(database);
+    }
 
-		function getTurnsByID(ID){
-			return TurnResource.show({id:ID}).$promise;
-		}
+    function getTurnsByID(ID) {
+      return TurnResource.show({id: ID}).$promise;
+    }
 
-		function createTurn(turn){
-			turn = angular.copy(turn);
-			turn.deadline = turn.deadline.getTime();
+    function createTurn(turn) {
+      turn = angular.copy(turn);
+      turn.deadline = turn.deadline.getTime();
 
-			return getAllTurns().add(turn);
-		}
+      return getAllTurns().$add(turn);
+    }
 
-		function updateTurn(turn){
-			return TurnResource.update(turn).$promise;
-		}
+    function updateTurn(turn) {
+      return TurnResource.update(turn).$promise;
+    }
 
-		function deleteTurn(turnID){
-			return TurnResource.delete({id:turnID}).$promise;
-		}
+    function deleteTurn(turnID) {
+      return TurnResource.delete({id: turnID}).$promise;
+    }
 
-	}
+  }
 })();
